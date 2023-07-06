83d3aa9e5ea9f3d2d4fe087d483b169a5ff6977c
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2023-07-06 18:11:38 +0200

    change delimiter

--- config.h
+++ config.h
@@ -2,6 +2,6 @@
 
 #define CLICKABLE_BLOCKS   1     // Enable clickability for blocks
 #define CMDLENGTH          50    // Trim block output to this length
-#define DELIMITER          " | " // Delimiter string used to separate blocks
+#define DELIMITER          " ┋ " // Delimiter string used to separate blocks
 #define LEADING_DELIMITER  1     // Whether a leading separator should be used
 #define TRAILING_DELIMITER 1     // Whether a leading separator should be used
